bcbe1e93012273f4b8998700276277b93a291c0f
malihatabassum19-1 <malihatabassum19-1@users.noreply.github.com>
2020-11-23 21:04:13 +0600

    final lab 1

---
+++ Acc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopf
+{
+    abstract class Acc
+    {
+        private string acNo;
+
+        public string AcNo
+        {
+            get { return acNo; }
+            set { acNo = value; }
+        }
+        private string acNm;
+
+        public string AcNm
+        {
+            get { return acNm; }
+            set { acNm = value; }
+        }
+        protected double bal;
+
+        public double Bal
+        {
+            get { return bal; }
+            set { bal = value; }
+        }
+        public Acc()
+        {
+
+        }
+        public Acc(string acNo, string acNm, double bal)
+        {
+            this.acNo = acNo;
+            this.acNm = acNm;
+            this.bal = bal;
+        }
+
+        virtual public void ShowInfo()
+        {
+            Console.WriteLine("Acc Name: " + accName);
+            Console.WriteLine("Acc No: " + accNo);
+            Console.WriteLine("Balance: " + balance);
+        }
+    }
+}
---
+++ OverDraft.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop
+{
+    class OverDraft
+    {
+        private double WithdrawMoney;
+
+        public double WithdrawMoney
+        {
+            get { return withdrawMoney; }
+            set { withdrawMoney = value; }
+        }
+        public Overdraft() { }
+        public Overdraft(string acNo, string acNm, double bal, double withdrwmoney) : base(acNo, acNm, bal)
+        {
+           
+        }
+
+
+        public override void Deposit(double amount)
+        {
+
+            if (bal + amount > 0)
+            {
+                bal += amount;
+            }
+
+
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (bal- amount < withdrawMoney)
+            {
+                bal -=  amount;
+            }
+            else
+            {
+                Console.WriteLine("No Balance");
+            }
+        }
+       
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+
+        }
+    }
+}
+    }
+}
